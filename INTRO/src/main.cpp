565ea96a19f1228bd474935af8fdc26247bdfcd9
FR3T0T <fr3t0t@gmail.com>
2024-09-12 11:07:58 +0200

    TEST2

---
+++ INTRO/src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if((digitalRead(P1_1)==0)){
+    digitalWrite(P1_0,HIGH);
+  }
+ // else
+  //  digitalWrite(P1_0)
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
