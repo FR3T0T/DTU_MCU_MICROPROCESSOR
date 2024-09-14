95ed1b52aac217582f002724f71afa5b52940281
FR3T0T <fr3t0t@gmail.com>
2024-09-15 00:59:17 +0200

    sdasd

--- INTRO/src/main.cpp
+++ INTRO/src/main.cpp
@@ -1,32 +1,24 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int switchPin = P2_1; 
+const int ledPin = P1_0;
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(P1_1,INPUT_PULLUP);
-  digitalWrite(P1_1,HIGH);
-  pinMode(P1_0,OUTPUT);
-  /*
-  R1REN |=BIT1;    //R1REN=R1REN|BIT1;
-  P1OUT |=BIT1;
-  */
-
+void setup()
+{
+  pinMode(switchPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(switchPin, HIGH);
 }
 
-void loop() {
+void loop()
+{
+  int switchState = digitalRead(switchPin);
 
-if((digitalRead(P1_1)==0)){
-  digitalWrite(P1_0,HIGH);
+  if (switchState == LOW) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
   }
-  else
-   digitalWrite(P1_0,LOW);
 
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(10);
 }
