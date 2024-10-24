6a2dc815233c5c2056a73225e4a122aa6f08b59b
FR3T0T <fr3t0t@gmail.com>
2024-10-24 10:54:37 +0200

    op

--- Opgave_3/src/main.cpp
+++ Opgave_3/src/main.cpp
@@ -1,130 +1,81 @@
-#include <msp430f5529.h>
-#include "ssd1306.h"
+#include <Arduino.h>
+#include <Energia.h>
 #include "i2c.h"
-#include <stdio.h>
+#include "ssd1306.h"
 
-#define DIP_SWITCH_VALUE (*((volatile unsigned char*) 0x0200)) // Example address for dip-switch input
-
-void pwm_init(void);
-void adc_init(void);
-void rc_filter_init(void);
-void timer_init(void);
-void adc_start_conversion(void);
-void adc_interrupt_enable(void);
-void oled_display_init(void);
-int main(void);
-
-// PWM Initialization
-void pwm_init(void)
+void init_ports()
 {
-    P2DIR |= BIT0;                // Set P2.0 as output
-    P2SEL |= BIT0;                // Select Timer A1 output for P2.0
-    TA1CCR0 = 4095;               // Set max count value (for 1 kHz PWM)
-    TA1CCTL1 = OUTMOD_7;          // Set output mode to Reset/Set
-    TA1CCR1 = TA1CCR0 / 2;        // Set initial duty cycle to 50%
-    TA1CTL = TASSEL_2 | MC_1 | TACLR; // SMCLK, Up mode, Clear timer
+    // put your setup code here, to run once:
+    P6SEL = 0x00;
+    P7SEL = 0;
+  
+    pinMode(P1_1, INPUT_PULLUP);
+    digitalWrite(P1_1, HIGH);
+    pinMode(P4_7, OUTPUT);
+    pinMode(P1_0, OUTPUT);
+    pinMode(P2_1, INPUT_PULLUP); // Pull_up på switch til P2.1 => aktiv low
+    digitalWrite(P2_1, HIGH);
+    P6DIR=0;
+    P6REN |= BIT6 | BIT5 | BIT4 | BIT3 | BIT2 | BIT1 | BIT0;
+    P6OUT |= BIT6 | BIT5 | BIT4 | BIT3 | BIT2 | BIT1 | BIT0;
+    P7REN |= BIT0;
+    P7OUT |= BIT0;
 }
 
-// ADC Initialization
-void adc_init(void)
+unsigned char Dip_switch = 0;
+char switch_value[20] = {0}; // der sættes plads af til tekstforklaring
+
+unsigned char n = 3,i=0;
+
+// for 4 Mhz SMCLK
+void init_SMCLK_XT2()
 {
-    ADC12CTL0 &= ~ADC12ENC;           // Disable ADC before configuration
-    ADC12CTL0 = ADC12SHT0_2 | ADC12ON; // Sample-and-hold time, ADC on
-    ADC12CTL1 = ADC12SHP;              // Use sampling timer
-    ADC12MCTL0 = ADC12INCH_12;         // Input channel A12 (P7.0)
-    P7SEL |= BIT0;                     // Configure P7.0 for ADC input
-    ADC12CTL0 |= ADC12ENC;             // Enable ADC
-}
+    WDTCTL = WDTPW|WDTHOLD;                     // Stop watchdog timer
+    P5SEL |= BIT2+BIT3;                         // Port select XT2
+    UCSCTL6 &= ~XT2OFF;                         // Enable XT2
+    UCSCTL4 |= SELA_2;                          // ACLK=REFO,SMCLK=DCO,MCLK=DCO
+    UCSCTL4 |= SELS_5 + SELM_5;                 // SMCLK=MCLK=XT2
 
-// Enable ADC Interrupt
-void adc_interrupt_enable(void)
-{
-    ADC12IE = ADC12IE0;               // Enable interrupt for ADC12MEM0
-}
-
-// RC Filter Initialization
-void rc_filter_init(void)
-{
-    // Configure RC filter output on P2.1
-    P2DIR |= BIT1;                // Set P2.1 as output for filtered PWM signal
-    P2SEL &= ~BIT1;               // Ensure P2.1 is in GPIO mode for analog filtering
-}
-
-// Timer A0 Initialization for Sampling Rate Control
-void timer_init(void)
-{
-    TA0CCR0 = 3999;                // Set Timer A0 period for 1 ms interrupts (assuming SMCLK at 4 MHz)
-    TA0CCTL0 = CCIE;               // Enable interrupt for Timer A0 CCR0
-    TA0CTL = TASSEL_2 | MC_1 | TACLR; // SMCLK, Up mode, Clear timer
-}
-
-// OLED Display Initialization
-void oled_display_init(void)
-{
-    ssd1306_init();                // Initialize SSD1306 OLED display
-    ssd1306_clearDisplay();        // Clear the display
-    // Removed call to ssd1306_display() as it's not defined in the provided library             // Update display with cleared content
-}
-
-// Start ADC Conversion
-void adc_start_conversion(void)
-{
-    ADC12CTL0 |= ADC12SC;          // Start ADC Conversion
-}
-
-// Main Program
-int main(void)
-{
-    WDTCTL = WDTPW | WDTHOLD;          // Stop watchdog timer
-    pwm_init();                        // Initialize PWM
-    adc_init();                        // Initialize ADC
-    rc_filter_init();                  // Initialize RC filter
-    timer_init();                      // Initialize Timer A0 for sampling rate control
-    adc_interrupt_enable();            // Enable ADC interrupt
-    oled_display_init();               // Initialize OLED display
-
-    unsigned int adc_value;
-    unsigned int duty_cycle;
-
-    __enable_interrupt();              // Enable global interrupts
-
-    while (1)
+    // Loop until XT1,XT2 & DCO stabilizes - in this case loop until XT2 settles
+    do
     {
-        // Calculate new duty cycle based on dip-switch value
-        duty_cycle = DIP_SWITCH_VALUE * 16; // Scale 8-bit dip-switch value to 12-bit range
-        TA1CCR1 = duty_cycle;           // Set PWM duty cycle
-
-        // Display duty cycle on OLED
-        ssd1306_setPosition(0, 0);     // Set cursor position
-        ssd1306_printUI32(0, 0, TA1CCR1, HCENTERUL_OFF);
-        __delay_cycles(40000);          // Delay for stability
-
-        // Measure and display filtered output
-        P2OUT ^= BIT1;                  // Toggle filtered output on P2.1 (for oscilloscope measurement)
+        UCSCTL7 &= ~(XT2OFFG + XT1LFOFFG + DCOFFG);     // Clear XT2,XT1,DCO fault flags
+        SFRIFG1 &= ~OFIFG;                              // Clear fault flags
     }
-} 
-
-// Timer A0 Interrupt Service Routine for Sampling Control
-#pragma vector = TIMER0_A0_VECTOR
-__interrupt void Timer_A(void)
-{
-    adc_start_conversion();              // Start ADC Conversion
+    while (SFRIFG1&OFIFG);                              // Test oscillator fault flag
 }
 
-// ADC Interrupt Service Routine for Reading ADC Value
-#pragma vector = ADC12_VECTOR
-__interrupt void ADC12_ISR(void)
+void init_pwm()
 {
-    if (ADC12IFG & ADC12IFG0)
+    TA1CTL|=ID_0|TASSEL_2|MC_1;     // Select  SMCLK as timer clock and up, no clock scaling
+    TA1CCTL1|= OUTMOD_3;            // Set/reset output  for P2.0  uses TA1CCR1 register for the duty cycle (TA1.1)
+    TA1CCR1 =25;                    // The comparevalue for when the output will be set /reset =>duty cycle
+    TA1CCR0=4095;                   // The top value for which the timer counter turns to decrement
+    pinMode(P2_0, OUTPUT);          // For getting the PWM signal out
+    P2SEL|=BIT0;                    // For PWM out -enable alternative function
+}
+
+int main()
+{
+    WDTCTL = WDTPW | WDTHOLD;   // Always disable watch for using delay function
+    init_SMCLK_XT2();           // Select the 4 MHz clock for SMCLK
+    init_ports();
+    init_pwm();
+ 
+    i2c_init();
+    ssd1306_init();
+    reset_diplay();
+    ssd1306_printText(0, 0, "hej med dig/0");
+    ssd1306_clearDisplay();
+
+    while(1)
     {
-        unsigned int adc_value = ADC12MEM0; // Get ADC result
-        ADC12IFG &= ~ADC12IFG0;             // Clear interrupt flag
-
-        // Use ADC value to control PWM duty cycle dynamically
-        TA1CCR1 = adc_value;                // Set PWM duty cycle based on ADC value
-
-        // Update OLED display
-        ssd1306_setPosition(0, 1);         // Set cursor position
-        ssd1306_printUI32(0, 1, adc_value, HCENTERUL_OFF);
+        n = (P6IN&0x7F);                                // & (BIT6 | BIT5 | BIT4 | BIT3 | BIT2 | BIT1 | BIT0)); ////digitalRead(P6_0)+digitalRead(P6_1)+digitalRead(P6_2)+digitalRead(P6_3)+digitalRead(P6_4)+digitalRead(P6_5)+digitalRead(P6_6);//+digitalRead(P7_0)<<8;
+        n=n + ((P7IN&BIT0)<<7);                         // Shift 7 times for biton msb place
+        sprintf(switch_value, "switch er: %03d\0", n);
+        ssd1306_printText(0, 0, switch_value);
+        ssd1306_printUI32(0,5,TA1CCR1,0);               // Write the contents of the compareregister setting the duty cycle
+ 
+        __delay_cycles(100000);
     }
 }
